7fe3f39f1cc5fa5aa4739e25ec885d76f512e689
EC2 Default User <108472584+seiya0507@users.noreply.github.com>
2022-07-10 06:29:16 +0000

    [Finish] Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,21 @@
 class BooksController < ApplicationController
+
   def new
     @book = Book.new
   end
 
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+       redirect_to book_path(@book.id)
+       flash[:notice] = "Book was successfully created."
+    else
+       @books = Book.all
+       render :index
+    end
   end
+
 
   def index
     @book = Book.new
@@ -23,16 +31,23 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(@book.id)
+       flash[:notice] = "Book was successfully updated."
+    else
+       render :edit
+    end
   end
 
+
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
+    @book = Book.find(params[:id])
+    @book.destroy
     redirect_to '/books'
+    flash[:notice] = "Book was successfully destroyed."
   end
 
 
